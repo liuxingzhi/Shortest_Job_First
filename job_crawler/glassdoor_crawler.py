df6674b792e57d624043f3262098c2a64e38ddc1
xingzhi <xingzhi2@illinois.edu>
2019-03-10 07:11:33 -0500

    爬虫 爬虫 还是多进程 哈哈哈

--- job_crawler/glassdoor_crawler.py
+++ job_crawler/glassdoor_crawler.py
@@ -1,5 +1,6 @@
 import selenium
-from typing import List, Dict
+from threading import Thread, Lock, Condition, Event
+from typing import List, Dict, Tuple, Sequence
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
@@ -13,9 +14,13 @@ from time import sleep
 from datetime import datetime, timedelta
 from collections import namedtuple
 import re
+from queue import Queue
+import threading
+import multiprocessing
+from multiprocessing import Manager, Process
 
 glassdoor_main_url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=software&sc.keyword=software&locT=&locId=&jobType="
-time_limit = 15
+time_limit = 12
 job_list_file = "job_list.txt"
 job_list = []
 
@@ -42,8 +47,36 @@ job_list = []
 #     password_tag.send_keys(password)
 #     driver.find_element_by_xpath("""//*[@id="LoginModal"]/div/div/div[2]/div/button//*[contains(text(), 'Sign In')]""")
 
+def get_brower():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')# 运行时关闭窗口
+    # 使用同一目录下的chromedriver进行模拟
+    browser_driver_address = str
+    if platform.system() == "Windows":
+        browser_driver_address = "chromedriver.exe"
+    elif platform.system() == "Darwin":
+        browser_driver_address = "chromedriver_2.45_mac"
+    elif platform.system() == "Linux":
+        browser_driver_address = "chromedriver_2.36_ubuntu16.04"
+    else:
+        browser_driver_address = "chromedriver_2.36_ubuntu16.04"
 
-def crawl_one_job_title(job: str):
+    driver_path = os.path.join(os.path.abspath("."), "browser_drivers", browser_driver_address)
+
+    print("chrome_drive:", driver_path)
+    driver = webdriver.Chrome(driver_path, chrome_options=chrome_options)
+    return driver
+
+
+def crawl_bunch_of_job(job_list: List[str], threadID: int):
+    print(threadID)
+    driver = get_brower()
+    for job in job_list:
+        crawl_one_job_title(job, driver)
+    driver.quit()
+
+
+def crawl_one_job_title(job: str, driver: webdriver.Chrome):
     # go to the main page of glassdoor
     driver.get(glassdoor_main_url)
     driver.implicitly_wait(time_limit)
@@ -55,9 +88,15 @@ def crawl_one_job_title(job: str):
     driver.find_element_by_id("HeroSearchButton").click()
 
     for i in range(20):
-        job_ul = driver.find_element_by_xpath("""//*[@id="MainCol"]/div/ul""")
+        try:
+            job_ul = driver.find_element_by_xpath("""//*[@id="MainCol"]/div/ul""")
+        except selenium.common.exceptions.NoSuchElementException as e:
+            # 如果没找到，那这个job很可能出错了，咱们跳过它
+            break
+
         li_list = job_ul.find_elements_by_tag_name("li")
         for li in li_list:
+            # li.click() 有可能会失败，如果发生这种情况，是glassdoor网页崩了，应该重新跑这个任务
             li.click()
             driver.implicitly_wait(time_limit)
 
@@ -79,12 +118,29 @@ def crawl_one_job_title(job: str):
             job_id = li.get_attribute("data-id")
             employer_id = li.get_attribute("data-emp-id")
             job_location = li.get_attribute("data-job-loc")
-            company_name = driver.find_element_by_xpath(
-                """//*[@id="HeroHeaderModule"]/div[3]/div[3]/a[contains(@class, 'empDetailsLink')]""").text
-            # get the posted date of a job, if the job have the attribute
+            driver.implicitly_wait(time_limit)
+
+            # 如果不能获取公司名称，说明这个glassdoor网页有问题，这份工作跳过不管了
+            try:
+                employer_wrapper_tag = driver.find_element_by_xpath(
+                    """//*[@id="HeroHeaderModule"]/div[contains(@class, 'empWrapper')]""")
+            except selenium.common.exceptions.StaleElementReferenceException as e:
+                continue
+
+            employer_wrapper_html = employer_wrapper_tag.get_attribute('innerHTML')
+            employer_wrapper_soup = BeautifulSoup(employer_wrapper_html, "html.parser")
+            company_name = employer_wrapper_soup.find('a', attrs={"class": "empDetailsLink"}).text
+            print(company_name)
+            # company_name_tag = driver.find_element_by_xpath(
+            #     """//*[@id="HeroHeaderModule"]/div[3]/div[3]/a""")
+            # # """//*[@id="HeroHeaderModule"]/div[3]/div[3]/a[contains(@class, 'empDetailsLink')]"""
+            # company_name = company_name_tag.text
+
+            # get the posted date of a job, if the job have the attribute, 找这个元素不需要等
+            driver.implicitly_wait(0)
             try:
                 # crawled post_time structure: n days ago
-                post_time_tag = li.find_element_by_xpath("""//span[contains(@class, 'minor')]""")
+                post_time_tag = li.find_element_by_xpath(""".//span[contains(@class, 'minor')]""")
             except selenium.common.exceptions.NoSuchElementException as e:
                 post_time_tag = None
 
@@ -98,6 +154,7 @@ def crawl_one_job_title(job: str):
                 # posted_time = datetime.now() - timedelta(days=days_ago)
                 posted_time = post_time_tag.text
 
+            driver.implicitly_wait(time_limit)
             job_description_container = driver.find_element_by_xpath("""//*[@id="JobDescriptionContainer"]""")
             description_div = job_description_container.find_element_by_class_name("jobDescriptionContent")
             job_description = description_div.text
@@ -109,7 +166,7 @@ def crawl_one_job_title(job: str):
             company_info["company_name"] = company_name
             company_website_url = None
 
-            driver.implicitly_wait(1) # this should be quick
+            driver.implicitly_wait(1)  # this should be quick
             try:
                 company_tag = driver.find_element_by_xpath(
                     """//*[@id="Details"]/div[2]/header//div[contains(@class, 'scrollableTabs')]//span[contains(text(), 'Company')]""")
@@ -128,7 +185,7 @@ def crawl_one_job_title(job: str):
                     value = span.text
                     company_info[key] = value
                 try:
-                    company_website_tag = basic_info_div.find_element_by_xpath("//a[contains(text(), 'Visit']")
+                    company_website_tag = basic_info_div.find_element_by_xpath(".//a[contains(text(), 'Visit']")
                 except selenium.common.exceptions.NoSuchElementException as e:
                     company_website_tag = None
                 if company_website_tag:
@@ -162,26 +219,31 @@ if __name__ == '__main__':
             job_list.append(line)
             line = f.readline()
 
-    chrome_options = Options()
-    # chrome_options.add_argument('--headless')# 运行时关闭窗口
-    # 使用同一目录下的chromedriver进行模拟
-    browser_driver_address = str
-    if platform.system() == "Windows":
-        browser_driver_address = "chromedriver.exe"
-    elif platform.system() == "Darwin":
-        browser_driver_address = "chromedriver_2.45_mac"
-    elif platform.system() == "Linux":
-        browser_driver_address = "chromedriver_2.36_ubuntu16.04"
-    else:
-        browser_driver_address = "chromedriver_2.36_ubuntu16.04"
+    pool_size = 4
+    pool = multiprocessing.Pool(pool_size)
+    total_num_job_titles = len(job_list)
+    one_thread_task = total_num_job_titles // 4
 
-    driver_path = os.path.join(os.path.abspath("."), "browser_drivers", browser_driver_address)
+    job_divisions = []
+    for i in range(pool_size):
+        if i == 0:
+            start = 0
+        else:
+            start = i * one_thread_task
 
-    print("chrome_drive:", driver_path)
-    driver = webdriver.Chrome(driver_path, chrome_options=chrome_options)
+        if i == pool_size:
+            end = one_thread_task
+        else:
+            end = (i + 1) * one_thread_task
 
-    # sign_in()
-    for job in job_list:
-        crawl_one_job_title(job)
+        division = job_list[int(start):int(end)]
+        job_divisions.append(division)
 
-    driver.quit()
+    processes = []
+    for i in range(pool_size):
+        pro = Process(target=crawl_bunch_of_job, args=(job_divisions[i], i), daemon=False)
+        processes.append(pro)
+        pro.start()
+
+    for i in range(pool_size):
+        processes[i].join()
