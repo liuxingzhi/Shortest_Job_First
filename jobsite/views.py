459ca96ed8daa638ad4686e99c3587a8b7876781
SISEP7 <jeff_illini_2019@outlook.com>
2019-03-22 18:14:42 -0500

    Revert "增加模板类"
    This reverts commit 01b0f16b

--- jobsite/views.py
+++ jobsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render
 from datetime import datetime
 from django.http.response import HttpResponse, JsonResponse, FileResponse, Http404
 from django.views.decorators.http import require_http_methods
@@ -10,28 +11,28 @@ from django.core.serializers.json import DjangoJSONEncoder
 # Create your views here.
 @require_http_methods(["GET"])
 def homeproc(request):
-    return render(request, "jobsite/index.html")
+    return render(request, "index.html")
 
 
 def career(request):
-    return render(request, "jobsite/career.html")
+    return render(request, "career.html")
 
 
 def tips(request):
-    return render(request, "jobsite/tips.html")
+    return render(request, "tips.html")
 
 
 def gpa(request):
-    return render(request, "jobsite/gpa.html")
+    return render(request, "gpa.html")
 
 
 def resume(request):
-    return render(request, "jobsite/resume.html")
+    return render(request, "resume.html")
 
 
 def programming(request):
-    return render(request, "jobsite/programming.html")
+    return render(request, "programming.html")
 
 
 def top(request):
-    return render(request, "jobsite/top.html")
+    return render(request, "top.html")
