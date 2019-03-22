01b0f16be17ce3880cafffb1c110b869ed776125
xingzhi <xingzhi2@illinois.edu>
2019-03-22 17:55:27 -0500

    增加模板类

--- jobsite/views.py
+++ jobsite/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from datetime import datetime
 from django.http.response import HttpResponse, JsonResponse, FileResponse, Http404
 from django.views.decorators.http import require_http_methods
@@ -11,28 +10,28 @@ from django.core.serializers.json import DjangoJSONEncoder
 # Create your views here.
 @require_http_methods(["GET"])
 def homeproc(request):
-    return render(request, "index.html")
+    return render(request, "jobsite/index.html")
 
 
 def career(request):
-    return render(request, "career.html")
+    return render(request, "jobsite/career.html")
 
 
 def tips(request):
-    return render(request, "tips.html")
+    return render(request, "jobsite/tips.html")
 
 
 def gpa(request):
-    return render(request, "gpa.html")
+    return render(request, "jobsite/gpa.html")
 
 
 def resume(request):
-    return render(request, "resume.html")
+    return render(request, "jobsite/resume.html")
 
 
 def programming(request):
-    return render(request, "programming.html")
+    return render(request, "jobsite/programming.html")
 
 
 def top(request):
-    return render(request, "top.html")
+    return render(request, "jobsite/top.html")
