94944452d679b3d520945e063739f149bece9750
liuxingzhi <liuxingzhi99@gmail.com>
2019-03-22 14:32:29 -0500

    Merge branch 'master' of https://github.com/liuxingzhi/411_team_project
    # Conflicts:
    #	job_crawler/glassdoor_crawler.py
    #	job_crawler/job_list.txt

--- Shortest_job_first/settings.py
+++ Shortest_job_first/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'Shortest_job_first.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '411project',
+        'USER': '309manager',
+        'PASSWORD': 'cs4112019',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
