9c2356cd4417501cb58e7f3dc92c2e654a90468d
xingzhi <xingzhi2@illinois.edu>
2019-03-29 03:25:29 -0500

    Merge branch 'master' of https://github.com/liuxingzhi/411_team_project into crawler_db_develop
    # Conflicts:
    #	job_crawler/glassdoor_crawler.py
    #	job_crawler/job_crawled.txt
    #	job_crawler/logo_crawler.py
    #	jobsite/models.py
    #	sqls/建表sql.sql
    #	sqls/练习查询.sql

--- jobsite/urls.py
+++ jobsite/urls.py
@@ -5,6 +5,7 @@ from users import views as user_views
 urlpatterns = [
     path("", views.homeproc),
     re_path(r"^career*", views.career),
+    re_path(r"^job_search", views.job_search, name="job_search"),
     re_path(r"^tips", views.tips),
     re_path(r"^gpa", views.gpa),
     re_path(r"^resume", views.resume),
